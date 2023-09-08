a248677b08173708e246af8693e26efd1627e0ee
Radhika <radhika.kondavaram0@walmart.com>
2023-09-08 15:30:44 +0530

    first commit

---
+++ src/main/java/com/stackroute/SpringRestDemo/service/EmployeeService.java
@@ -0,0 +1,100 @@
+package com.stackroute.SpringRestDemo.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stackroute.SpringRestDemo.exception.EmployeeAlreadyExistsException;
+import com.stackroute.SpringRestDemo.exception.EmployeeNotFoundException;
+import com.stackroute.SpringRestDemo.model.Employee;
+import com.stackroute.SpringRestDemo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService implements IEmployeeService{
+	
+	@Autowired
+	private EmployeeRepository Erepo;
+	
+//	public EmployeeService()
+//	{
+//		
+//	}
+	
+//	@Autowired
+//	public EmployeeService(EmployeeRepository e)
+//	{
+//		this.Erepo=e;
+//	}
+	@Override
+	public Employee addEmployee(Employee emp)throws EmployeeAlreadyExistsException
+	{
+		Optional<Employee> opt=this.Erepo.findById((long) emp.getId());
+		if(opt.isPresent()) {
+			throw new EmployeeAlreadyExistsException();
+		}
+		Erepo.save(emp);
+		return emp;
+	}
+	public Employee getEmployeeById(int id)throws EmployeeNotFoundException
+	{
+		Employee emp=null;
+		Optional<Employee> opt=this.Erepo.findById((long) id);
+	
+		if(opt.isPresent()) {
+			emp=opt.get();
+			
+		}
+		else
+		
+			throw new EmployeeNotFoundException();
+		return emp;	
+		
+}
+	
+	@Override
+	public List getAllEmployees()
+	{
+		return Erepo.findAll();
+	}
+	
+	@Override
+	public boolean deleteEmployee(int id)throws EmployeeNotFoundException{
+		
+	boolean status=false;
+	Optional<Employee> opt=this.Erepo.findById((long) id);
+	if(opt.isPresent()) {
+		Employee emp=opt.get();
+		Erepo.delete(emp);
+		status=true;
+		
+	}
+	else 
+		throw new EmployeeNotFoundException();
+		return status;	
+			
+}
+	
+	
+	@Override
+	public Employee UpdateEmployee(int id)throws EmployeeNotFoundException{
+		
+		Employee emp=null;
+	Optional<Employee> opt=this.Erepo.findById((long) id);
+	if(opt.isPresent()) {
+		 emp=opt.get();
+		emp.setFirstname("U-Next");
+		emp.setLastname("Bangalore");
+		Erepo.save(emp);
+		
+		
+	}
+	else 
+		throw new EmployeeNotFoundException();
+		return emp;	
+			
+}
+	}
+
